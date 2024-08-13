fcbd67d83b9af5e71f17f6176d6219e391678ccd
Igor Mezin <imezin@ds-in.net>
2024-08-13 15:32:54 +0300

    (refs: #1) implemented get method for list of users

--- showcase backend/main.cpp
+++ showcase backend/main.cpp
@@ -55,6 +55,30 @@ int main()
 
     CROW_ROUTE(app, "/users").methods("GET"_method)
         ([&](const crow::request& req, crow::response& res) {
+        if (req.method == "GET"_method) {
+            std::vector<User> users = get_all_users_from_db(hDbc);
+            std::string template_content = read_file("users.html");
+            std::string user_rows;
+
+            for (const auto& user : users) {
+                user_rows += "<tr>"
+                    "<td>" + user.fio + "</td>"
+                    "<td>" + user.specialty + "</td>"
+                    "<td>" + user.subjectScores + "</td>"
+                    "<td>" + user.study_type + "</td>"
+                    "</tr>";
+            }
+
+            // Вставка строк пользователей в шаблон
+            size_t pos = template_content.find("<!-- USER_ROWS_MARKER -->");
+            if (pos != std::string::npos) {
+                template_content.replace(pos, std::string("<!-- USER_ROWS_MARKER -->").length(), user_rows);
+            }
+
+            res.set_header("Content-Type", "text/html; charset=UTF-8");
+            res.write(template_content);
+            res.end();
+        }
         });
 
 
